4b3aefc1c872ea699c73f326738c3d565f1486de
Adam Babcock <github@mrhen.com>
2019-08-25 14:21:39 -0500

    Clone from playground

---
+++ src/e2e/servers.ts
@@ -0,0 +1,12 @@
+export default {
+  'express-server': {
+    host: process.env['EXPRESS_SERVER_HOST'],
+    port: process.env['EXPRESS_SERVER_PORT'] || '3000',
+    protocol: process.env['EXPRESS_SERVER_PROTOCOL'] || 'http',
+
+    supports: {
+      hello: true,
+      missing: true,
+    },
+  },
+}
